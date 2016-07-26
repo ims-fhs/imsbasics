4b4dccb67919db5e5020132c01bb87333d4c74e2
Christoph Strauss <christoph.strauss@fhsg.ch>
2016-07-26 09:15:58 +0200

    Add functions dict, int_dict, g2e and e2g from schedule package.

--- R/imsbasics.R
+++ R/imsbasics.R
@@ -426,3 +426,88 @@ load_fom_archive <- function(short_uuid, path2archive) {
   }
 }
 
+#' Define a German <-> English dictionary for weekday and month names
+#'
+#' @return A dictionary as list (subset possible)
+#' @export
+dict <- function() { # Not used anymore
+  dict_g2e <- setNames(
+    c("Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Jan", "Feb", "Mar",
+      "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"),
+    c("So", "Mo", "Di", "Mi", "Do", "Fr", "Sa", "Jan", "Feb", "Mar",
+      "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"))
+
+  dict_e2g <- setNames(
+    c("So", "Mo", "Di", "Mi", "Do", "Fr", "Sa", "Jan", "Feb", "Mar", "Apr",
+      "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"),
+    c("Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Jan", "Feb", "Mar", "Apr",
+      "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"))
+  return(list(g2e = dict_g2e, e2g = dict_e2g))
+}
+
+#' Define a Integer-alias <-> English dictionary for weekday and month names
+#'
+#' @return A dictionary as list (subset possible)
+#' @export
+int_dict <- function() { # This is used for Jan -> integer conversion
+  dict_int2e  <- setNames(
+    c("Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Jan", "Feb", "Mar",
+      "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"),
+    c("07", "01", "02", "03", "04", "05", "06", "01", "02", "03",
+      "04", "05", "06", "07", "08", "09", "10", "11", "12"))
+
+  dict_e2int <- setNames(
+    c("07", "01", "02", "03", "04", "05", "06", "01", "02", "03",
+      "04", "05", "06", "07", "08", "09", "10", "11", "12"),
+    c("Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Jan", "Feb", "Mar",
+      "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"))
+  return(list(e2int = dict_e2int, int2e = dict_int2e))
+}
+
+#' German => English translator based on dict()
+#'
+#' @param ger_expr A character (in german)
+#'
+#' @return A character, the translation
+#' @export
+g2e <- function(ger_expr) {
+  if (!exists("dict_g2e")) {
+    # dictg_g2e <- dict()$g2e
+    assign("dict_g2e", dict()$g2e, envir = .GlobalEnv)
+  }
+  res <- character(length(ger_expr))
+  for (i in 1:length(ger_expr)) {
+    split <- unlist(strsplit(ger_expr[i], split = ", "))
+    assertthat::assert_that(all(split %in% names(dict_g2e)))
+    if (length(split) == length(ger_expr[i])) {
+      res[i] <- dict_g2e[split]
+    } else {
+      res[i] <- paste(dict_g2e[split], collapse = ", ")
+    }
+  }
+  return(res)
+}
+
+#' English => German translator based on dict()
+#'
+#' @param eng_expr A character (in english)
+#'
+#' @return A character, the translation
+#' @export
+e2g <- function(eng_expr) {
+  if (!exists("dict_e2g")) {
+    assign("dict_e2g", dict()$e2g, envir = .GlobalEnv)
+  }
+  res <- character(length(eng_expr))
+  for (i in 1:length(eng_expr)) {
+    split <- unlist(strsplit(eng_expr[i], split = ", "))
+    assertthat::assert_that(all(split %in% names(dict_e2g)))
+    if (length(split) == length(eng_expr[i])) {
+      res[i] <- dict_e2g[split]
+    } else {
+      res[i] <- paste(dict_e2g[split], collapse = ", ")
+    }
+  }
+  return(res)
+}
+
